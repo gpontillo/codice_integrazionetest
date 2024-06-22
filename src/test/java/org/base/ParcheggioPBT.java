8fc7b691ab37f89f8b2056aa8c3f8522864935d3
Giovanni Pontillo <g.pontillo1@studenti.uniba.it>
2024-06-22 12:04:48 +0200

    Add tests for ParcheggioPBT

--- src/test/java/org/base/ParcheggioPBT.java
+++ src/test/java/org/base/ParcheggioPBT.java
@@ -2,50 +2,99 @@ package org.base;
 
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.*;
+import net.jqwik.api.statistics.Statistics;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParcheggioPBT {
     @Property
     @Report(Reporting.GENERATED)
-    void testCostoMinimoOreUgualeNumeroMinimoOre(
-            @ForAll @IntRange(min = 0, max = 24) int numeroMinimoOre) {
+    void costoMinimoRispettato(
+            @ForAll("oreParcheggioMinoriUguali") OreCombinate oreCombinate) {
 
-        Parcheggio parcheggio = new Parcheggio(numeroMinimoOre, 0);
+        Statistics.label("oreParcheggio").collect(oreCombinate.oreParcheggio);
+        Statistics.label("numeroMinimoOre").collect(oreCombinate.numeroMinimoOre);
+        Statistics.label("Minori o uguali").collect(oreCombinate.oreParcheggio < oreCombinate.numeroMinimoOre ? "Minore" : "Uguale");
 
-        assertEquals(2.0, parcheggio.calcolaCostoParcheggio(numeroMinimoOre));
+        Parcheggio parcheggio = new Parcheggio(oreCombinate.numeroMinimoOre, 0);
+
+        assertEquals(2.0, parcheggio.calcolaCostoParcheggio(oreCombinate.oreParcheggio));
     }
 
     @Property
     @Report(Reporting.GENERATED)
-    void testCostoSuperioreNumeroMinimoOre(
-            @ForAll @IntRange(min = 0, max = 23) int numeroMinimoOre,
-            @ForAll @IntRange(min = 1, max = 24) int oreParcheggio) {
+    void costoSuperioreNumeroMinimoOre(
+            @ForAll("oreParcheggioMaggiori") OreCombinate oreCombinate) {
 
-        // Ensuring oreParcheggio is greater than numeroMinimoOre for the test to be valid
-        Assume.that(oreParcheggio > numeroMinimoOre);
+        Statistics.label("oreParcheggio").collect(oreCombinate.oreParcheggio);
+        Statistics.label("numeroMinimoOre").collect(oreCombinate.numeroMinimoOre);
+        Statistics.label("Differenza").collect(oreCombinate.oreParcheggio - oreCombinate.numeroMinimoOre);
 
-        Parcheggio parcheggio = new Parcheggio(numeroMinimoOre, 0);
+        Parcheggio parcheggio = new Parcheggio(oreCombinate.numeroMinimoOre, 0);
 
-        double expectedCost = 2.0 + ((oreParcheggio - numeroMinimoOre) * 0.5);
-        assertEquals(expectedCost, parcheggio.calcolaCostoParcheggio(oreParcheggio));
+        double expectedCost = 2.0 + ((oreCombinate.oreParcheggio - oreCombinate.numeroMinimoOre) * 0.5);
+        assertEquals(expectedCost, parcheggio.calcolaCostoParcheggio(oreCombinate.oreParcheggio));
     }
 
     @Property
     @Report(Reporting.GENERATED)
-    void testCostoMassimoApplicato(
-            @ForAll @IntRange(min = 0, max = 23) int numeroMinimoOre,
-            @ForAll @IntRange(min = 1, max = 24) int oreParcheggio,
-            @ForAll @DoubleRange(min = 0.0, max = 100.0) double costoMassimo) {
+    void costoMassimoApplicato(
+            @ForAll("oreParcheggioMaggiori") OreCombinate oreCombinate,
+            @ForAll @DoubleRange(min = 0.0, max = 14.0) double costoMassimo) {
+        Parcheggio parcheggio = new Parcheggio(oreCombinate.numeroMinimoOre, costoMassimo);
 
-        // Ensuring oreParcheggio is greater than numeroMinimoOre for the test to be valid
-        Assume.that(oreParcheggio > numeroMinimoOre);
-
-        Parcheggio parcheggio = new Parcheggio(numeroMinimoOre, costoMassimo);
-
-        double expectedCost = 2.0 + ((oreParcheggio - numeroMinimoOre) * 0.5);
+        double expectedCost = 2.0 + ((oreCombinate.oreParcheggio - oreCombinate.numeroMinimoOre) * 0.5);
         if (costoMassimo > 0) {
             expectedCost = Math.min(expectedCost, costoMassimo);
         }
+
+        Statistics.label("oreParcheggio").collect(oreCombinate.oreParcheggio);
+        Statistics.label("costoMassimo").collect(costoMassimo);
+        Statistics.label("Supera?").collect(expectedCost > costoMassimo);
+
+        assertEquals(expectedCost, parcheggio.calcolaCostoParcheggio(oreCombinate.oreParcheggio));
+    }
+
+    @Property
+    @Report(Reporting.GENERATED)
+    void calcoloCostoSenzaCondizioniDati(
+            @ForAll @DoubleRange(min = 0, max = 23) int numeroMinimoOre,
+            @ForAll @DoubleRange(min = 1, max = 24) int oreParcheggio,
+            @ForAll @DoubleRange(min = 0.0, max = 14.0) double costoMassimo) {
+        Parcheggio parcheggio = new Parcheggio(numeroMinimoOre, costoMassimo);
+
+        double expectedCost = oreParcheggio > numeroMinimoOre ? 2.0 + ((oreParcheggio - numeroMinimoOre) * 0.5) : 2.0;
+        if (costoMassimo > 0) {
+            expectedCost = Math.min(expectedCost, costoMassimo);
+        }
+
         assertEquals(expectedCost, parcheggio.calcolaCostoParcheggio(oreParcheggio));
+    }
+
+    @Provide
+    Arbitrary<OreCombinate> oreParcheggioMaggiori() {
+        Arbitrary<Integer> numeroMinimoOre = Arbitraries.integers().between(0, 23);
+        Arbitrary<Integer> oreParcheggio = Arbitraries.integers().between(1, 24);
+
+        return Combinators.combine(numeroMinimoOre, oreParcheggio).as(OreCombinate::new).filter(k -> k.oreParcheggio > k.numeroMinimoOre);
+    }
+
+    @Provide
+    Arbitrary<OreCombinate> oreParcheggioMinoriUguali() {
+        Arbitrary<Integer> numeroMinimoOre = Arbitraries.integers().between(0, 23);
+        Arbitrary<Integer> oreParcheggio = Arbitraries.integers().between(1, 24);
+
+        return Combinators.combine(numeroMinimoOre, oreParcheggio).as(OreCombinate::new).filter(k -> k.oreParcheggio <= k.numeroMinimoOre);
+    }
+
+    // Classe usata per definire la condizione per la generazione dei parametri
+    private class OreCombinate {
+        public int numeroMinimoOre;
+        public int oreParcheggio;
+
+        public OreCombinate(int numeroMinimoOre, int oreParcheggio) {
+            this.numeroMinimoOre = numeroMinimoOre;
+            this.oreParcheggio = oreParcheggio;
+        }
     }
 }
